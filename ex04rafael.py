a9d1352da1931553e5cc34de841feebe650de442
Rafael Milagre <rafaelmilagrearquiteto@gmail.com>
2025-07-01 18:02:46 -0300

    commit ex04

---
+++ ex04rafael.py
@@ -0,0 +1,4 @@
+num = int(input('Número do funcionário: '))
+horas = int(input('Horas trabalhadas: '))
+vhora = float(input('Valor hora: '))
+print(f'O funcionário número {num}, \nrecebe o salário de R$ {horas*vhora:.2f}.')
